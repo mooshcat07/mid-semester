b4d7dbfeb0575b4c6331378c7243339f1fce86dc
akuzike <akuzikefoxxmisomali@gmail.com>
2025-08-06 05:51:42 +0200

    Answered question 1

---
+++ Question1.js
@@ -0,0 +1,21 @@
+// number
+let age = 20;
+// string
+let fullname = "Akuzike Amos Misomali";
+// boolean
+let isStudent = true;
+// undefined
+let certificate;
+// object
+let grades = [85, 78, 90, 87, 99, 100];
+// function
+let Eat = () => {
+    return 'Student eat';
+}
+
+console.log(typeof age);
+console.log(typeof fullname);
+console.log(typeof isStudent);
+console.log(typeof certificate);
+console.log(typeof grades);
+console.log(typeof Eat);
