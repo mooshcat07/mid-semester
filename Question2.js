eee0ecbf1415175adc1b9ffeccbe1aa7e5daa3c2
akuzike <akuzikefoxxmisomali@gmail.com>
2025-08-06 05:58:17 +0200

    Answred Question2

---
+++ Question2.js
@@ -0,0 +1,11 @@
+const PI = 3.14;
+let fullname = 'Akuzike';
+var username = 'Mooshcat';
+
+// const variables
+// PI = 22;    /* Will crash since constants cannot be reassigned after initial declaration  */
+console.log(PI);        
+
+// let variables
+fullname = 'Atlas';
+console.log(fullname);     /* Value can be changed without crashing the program  */
